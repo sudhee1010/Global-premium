505ec68af6960fa82728139b6281e1850d96cac9
sudhee krishna <sudheekrishna100@gmail.com>
2026-03-11 10:37:40 +0530

    admin latest setup

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AdminLayout } from "./layout/AdminLayout";
 import Dashboard from "./pages/Dashboard";
-import Products from "./pages/Products";
+// import Products from "./pages/Products";
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
 
         <Route element={<AdminLayout />}>
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/products" element={<Products />} />
+          {/* <Route path="/products" element={<Products />} /> */}
         </Route>
 
       </Routes>
---
+++ admin/src/pages/Dashboard.jsx
@@ -0,0 +1,5 @@
+const Dashboard = () => {
+  return <div className="text-2xl font-bold">Dashboard Page</div>;
+};
+
+export default Dashboard;
